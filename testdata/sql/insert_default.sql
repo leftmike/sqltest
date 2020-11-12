00b33356a5e57f2a6f2abca83b75e6d134605a7b
leftmike <mikemon@gmail.com>
2020-11-12 09:12:22 -0800

    improve insert tests with default and null

--- testdata/sql/insert_default.sql
+++ testdata/sql/insert_default.sql
@@ -17,3 +17,43 @@ SELECT c1, c2 FROM tbl1;
 INSERT INTO tbl1 (c2, c1) VALUES (DEFAULT, DEFAULT);
 
 SELECT c1, c2 FROM tbl1;
+
+INSERT INTO tbl1 VALUES
+    ('1st', DEFAULT),
+    (NULL, 789),
+    (DEFAULT, NULL);
+
+SELECT c1, c2 FROM tbl1;
+
+DROP TABLE IF EXISTS tbl2;
+
+CREATE TABLE tbl2 (
+    c1 int default 1,
+    c2 int default 2 not null,
+    c3 int not null);
+
+INSERT INTO tbl2 VALUES
+    (10, 20, 30),
+    (20, 30, 40),
+    (NULL, DEFAULT, 50),
+    (DEFAULT, 40, 60);
+
+SELECT c1, c2, c3 FROM tbl2;
+
+{{Fail .Test}}
+INSERT INTO tbl2 VALUES
+    (DEFAULT, NULL, 10);
+
+{{Fail .Test}}
+INSERT INTO tbl2 VALUES
+    (NULL, DEFAULT, NULL);
+
+{{Fail .Test}}
+INSERT INTO tbl2 (c1) VALUES (10);
+
+{{Fail .Test}}
+INSERT INTO tbl2 VALUES (10, 20, DEFAULT);
+
+INSERT INTO tbl2 (c3) VALUES (70);
+
+SELECT c1, c2, c3 FROM tbl2;
