d0dc7c38a56f8f1b8e8c9c480a92610d186809d6
leftmike <mikemon@gmail.com>
2020-11-24 12:45:29 -0800

    test remaining column types

--- testdata/sql/types.sql
+++ testdata/sql/types.sql
@@ -13,5 +13,40 @@ INSERT INTO tbl1 VALUES
     (-7, -8, -9, -10, -11, -12),
     (13, 14, 15, 16, 17, 18);
 
-SELECT * FROM tbl1;
+SELECT c1, c2, c3, c4, c5, c6 FROM tbl1;
 
+DROP TABLE IF EXISTS tbl2;
+
+CREATE TABLE tbl2 (c1 double precision, c2 real);
+
+INSERT INTO tbl2 VALUES
+    (1.23, 4.5678),
+    (-7.891, -2.345);
+
+SELECT c1, c2 FROM tbl2;
+
+DROP TABLE IF EXISTS tbl3;
+
+CREATE TABLE tbl3 (c1 bool);
+
+INSERT INTO tbl3 VALUES (true), (false);
+
+SELECT c1 FROM tbl3;
+
+DROP TABLE IF EXISTS tbl4;
+
+CREATE TABLE tbl4 (c1 char, c2 char(2), c3 varchar(5), c4 text);
+
+INSERT INTO tbl4 VALUES
+    ('a', 'bb', 'ccc', 'dddd'),
+    ('A', 'BB', 'CCC', 'DDDD');
+
+SELECT c1, c2, c3, c4 FROM tbl4;
+
+DROP TABLE IF EXISTS tbl5;
+
+CREATE TABLE tbl5 (c1 BYTEA);
+
+INSERT INTO tbl5 VALUES ('A'), ('bb'), ('CCC');
+
+SELECT c1 FROM tbl5;
